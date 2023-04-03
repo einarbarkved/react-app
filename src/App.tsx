29d7e81628ddafb92e86ece9e327631f1f00d32c
einarbarkved <einar.barkved@gmail.com>
2023-04-03 21:21:50 +0200

    Day 1.1 30%

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
 import { useState } from "react";
-import Alert from "./components/Alert";
-import Button from "./components/Button";
-import ListGroup from "./components/ListGroup";
+import Like from "./components/Like";
+
 function App() {
   const items = ["Sau", "Ku"];
   const [alertVisible, setAlertVisibility] = useState(false);
   return (
     <div>
-      {alertVisible && (
-        <Alert onClose={() => setAlertVisibility(false)}>Hello World</Alert>
-      )}
-      <Button color="warning" onClick={() => setAlertVisibility(true)}>
-        Click meeee
-      </Button>
-      <ListGroup
-        items={items}
-        heading="Hello"
-        onSelectItem={() => console.log("Clikkd")}
-      ></ListGroup>
+      <Like onClick={() => console.log("clicked")} />
     </div>
   );
 }
