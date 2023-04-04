ae363cc9365441e1c59dbdfb2eb2126ea3910105
Einar Barkved <einar.barkved@gmail.com>
2023-04-04 09:45:31 +0200

    Handling form submission

--- README.md
+++ README.md
@@ -8,9 +8,10 @@ This project is created using `npm create vite@latest`
 2. Run `npm i` to install modules from package.json
 3. Run `npm run dev` to start web-server
 
-#### immer "produce" is used to set nested states easily:
+#### Immer "produce" is used to set nested states easily:
 
 ```typescript
+import produce from "immer";
 const [bugs, setBugs] = useState([
   { id: 1, title: "Bug 1", fixed: false },
   { id: 2, title: "Bug 2", fixed: false },
@@ -27,6 +28,7 @@ const handleClick = () => {
 ```
 
 ```typescript
+import produce from "immer";
 const [game, setGame] = useState({
   id: 1,
   player: {
--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,11 @@
 import { useState } from "react";
-import produce from "immer";
-import NavBar from "./components/NavBar";
-import Cart from "./components/Cart";
+import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 function App() {
-  const [game, setGame] = useState({
-    id: 1,
-    player: {
-      name: "John",
-      gear: ["Gloves"],
-    },
-  });
-
-  const handleClick = () => {
-    setGame(
-      produce((draft) => {
-        draft.player.name = "Einar";
-      })
-    );
-    setGame(
-      produce((draft) => {
-        draft.player.gear.push("Boots");
-      })
-    );
-  };
-
   return (
     <div>
-      {game.player.name}
-      {game.player.gear.map((item) => (
-        <p key={item}>{item}</p>
-      ))}
-      <button onClick={handleClick}>Click</button>
+      <Form />
     </div>
   );
 }
